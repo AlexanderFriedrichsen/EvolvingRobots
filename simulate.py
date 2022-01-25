f15495ad39b155a0d3cd9db40ef47de8d7ab3f65
AelaleA37 <alex.p.friedrichsen@gmail.com>
2022-01-25 12:20:54 -0500

    make a sensor

--- simulate.py
+++ simulate.py
@@ -1,5 +1,6 @@
 import pybullet as p
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 import time 
 # import sys
 # print(sys.version)
@@ -13,9 +14,12 @@ p.setGravity(0,0,-9.8)
 # Make a plane for objeccts to rest on
 planeId = p.loadURDF("plane.urdf")
 p.loadSDF("world.sdf")
-robotID = p.loadURDF("body.urdf")
+robotId = p.loadURDF("body.urdf")
+
+pyrosim.Prepare_To_Simulate(robotId)
 for i in range(10000):
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     time.sleep(0.01)
     print(i)
 p.disconnect()
