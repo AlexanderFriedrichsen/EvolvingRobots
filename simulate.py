b1313f8315b1448a65a518fd5f11b83a36d6606a
AelaleA37 <alex.p.friedrichsen@gmail.com>
2022-02-01 12:35:11 -0500

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -12,7 +12,7 @@ p.setGravity(0,0,-9.8)
 
 # Make a plane for objects to rest on, world for objects, body of robot
 planeId = p.loadURDF("plane.urdf")
-worldId = p.loadSDF("world.sdf")
+p.loadSDF("world.sdf")
 robotId = p.loadURDF("body.urdf")
 
 backLegSensorValues = np.zeros(500)
@@ -23,7 +23,7 @@ for i in range(500):
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     time.sleep(0.01)
-    print(i)
+    #print(i)
 p.disconnect()
 
 #sensor outputs
