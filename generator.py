57f83cb898714ac74d176d979b0ac1c53d7aef14
AelaleA37 <alex.p.friedrichsen@gmail.com>
2022-02-14 21:44:53 -0500

    updated motors

--- generator.py
+++ generator.py
@@ -8,8 +8,8 @@ def Create_World():
     height = 1
     width = 1
     # position  - a link start with its exact center at these cords.
-    x = -2
-    y = 2
+    x = -3
+    y = 3
     z = .5
     
     pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
@@ -24,9 +24,9 @@ def Create_Robot():
     # position  - a link start with its exact center at these cords.
     x = 0
     y = 0
-    z = 1.5
+    z = 0.5
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[torsoLength,torsoWidth,torsoHeight])
+    pyrosim.Send_Cube(name="Torso", pos=[x,y,z+1] , size=[torsoLength,torsoWidth,torsoHeight])
     
     # legs size
     legLength = 1
@@ -35,13 +35,13 @@ def Create_Robot():
 
     #create BackLeg
     pyrosim.Send_Joint(name="Torso_BackLeg" , parent="Torso", child="BackLeg",
-                       type = "revolute", position = [0,-.5,1])
-    pyrosim.Send_Cube(name="BackLeg", pos=[0,-.5,-.5] , size=[legLength, legWidth, legHeight])
+                       type = "revolute", position =[.5,0,1])#".5 0 1") # 
+    pyrosim.Send_Cube(name="BackLeg", pos=[0.5,0,-.5] , size=[legLength, legWidth, legHeight])
     
     #create FrontLeg
     pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg", 
-                       type = "revolute", position = [0,.5,1])
-    pyrosim.Send_Cube(name="FrontLeg", pos=[0,.5,-.5] , size=[legLength,legWidth,legHeight])
+                       type = "revolute", position = [-.5,0,1]) #"-.5 0 1") # 
+    pyrosim.Send_Cube(name="FrontLeg", pos=[-.5,0,-.5] , size=[legLength,legWidth,legHeight])
    
 
     
--- simulate.py
+++ simulate.py
@@ -3,6 +3,7 @@ import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import time 
 import numpy as np
+import random
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -12,9 +13,23 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 p.loadSDF("world.sdf")
 robot = p.loadURDF("body.urdf")
-NB_LOOPS = 500
+NB_LOOPS = 2000
 backLegSensorValues = np.zeros(NB_LOOPS)
 frontLegSensorValues = np.zeros(NB_LOOPS)
+
+FrontLeg_amplitude = np.pi/4.0
+FrontLeg_frequency = 20
+FrontLeg_phaseOffset = 0
+
+BackLeg_amplitude = np.pi/4.0
+BackLeg_frequency = 20
+BackLeg_phaseOffset = np.pi/4
+
+targetAngles = np.linspace(-np.pi/4, np.pi/4, NB_LOOPS)
+FrontLeg_motorCommand = FrontLeg_amplitude * np.sin(FrontLeg_frequency * targetAngles + FrontLeg_phaseOffset)
+BackLeg_motorCommand = BackLeg_amplitude * np.sin(BackLeg_frequency * targetAngles + BackLeg_phaseOffset)
+
+
 pyrosim.Prepare_To_Simulate(robot)
 for i in range(NB_LOOPS):
     p.stepSimulation()
@@ -23,13 +38,23 @@ for i in range(NB_LOOPS):
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 
-    # # motorize
-    # pyrosim.Set_Motor_For_Joint(
-    # bodyIndex = robot,
-    # jointName = "Torso_BackLeg",
-    # controlMode = p.POSITION_CONTROL,
-    # targetPosition = 0.0,
-    # maxForce = 500) #(in Newton-metres)
+
+
+    # motorize Torso_BackLeg
+    pyrosim.Set_Motor_For_Joint(
+    bodyIndex = robot,
+    jointName = "Torso_BackLeg",
+    controlMode = p.POSITION_CONTROL,
+    targetPosition = BackLeg_motorCommand[i],
+    maxForce = 20) #(in Newton-metres)
+    
+    # motorize Torso_FrontLeg
+    pyrosim.Set_Motor_For_Joint(
+    bodyIndex = robot,
+    jointName = "Torso_FrontLeg",
+    controlMode = p.POSITION_CONTROL,
+    targetPosition = FrontLeg_motorCommand[i],
+    maxForce = 20) #(in Newton-metres)
     
     # pyrosim.Set_Motor_For_Joint(
     # bodyIndex = robot,
@@ -39,10 +64,14 @@ for i in range(NB_LOOPS):
     # maxForce = 500) #(in Newton-metres)
     
 
-    time.sleep(0.01)
+    time.sleep(1/240)
     #print(i)
 p.disconnect()
 
 #sensor outputs
 np.save("data/backLegSensorValues.npy", backLegSensorValues)
 np.save("data/frontLegSensorValues", frontLegSensorValues)
+np.save('data/targetAngles.npy',targetAngles)
+np.save('data/BackLeg_motorCommand.npy',BackLeg_motorCommand)
+np.save('data/FrontLeg_motorCommand.npy',FrontLeg_motorCommand)
+exit()
